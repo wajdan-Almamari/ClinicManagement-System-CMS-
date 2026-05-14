eb47dcc1e2a61f98bec9d2a12152c54d2820e473
wajdan <wejdan95.almamari@gmail.com>
2026-05-14 13:49:30 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicManagement_System_CMS_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
